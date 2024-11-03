5af31da61c0d863135bd5eeb4c677d15fba1cba6
Leonardo Lima <leonardo.b.lima1@gmail.com>
2024-11-03 15:06:06 -0300

    alteracao tipo de grafico

--- src/Components/Templates/HomeTemplate/index.tsx
+++ src/Components/Templates/HomeTemplate/index.tsx
@@ -141,21 +141,20 @@ export const HomeTemplate = () => {
         <Card>
           <S.TitleCard>Comparativo de remuneração (R$)</S.TitleCard>
           <ResponsiveContainer width={"100%"} height={400}>
-            <BarChart
-              data={dataBar}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
+            <LineChart data={dataLinear}>
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
+              {/* <XAxis dataKey="name" padding={{ left: 30, right: 30 }} /> */}
               <YAxis />
               <Tooltip />
-              <Bar dataKey="uv" />
-            </BarChart>
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="pv"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
           </ResponsiveContainer>
         </Card>
       </S.Container>
